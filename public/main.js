0815c41df2238a803c9737203e90bbdff9a58617
mooamro <mohamadamro262@gmail.com>
2025-06-21 14:42:40 +0200

    Update main.js

--- public/main.js
+++ public/main.js
@@ -30,14 +30,16 @@ function switchTurn() {
 
 function checkWinner() {
   const winCombos = [
-    [0,1,2], [3,4,5], [6,7,8], // Reihen
-    [0,3,6], [1,4,7], [2,5,8], // Spalten
-    [0,4,8], [2,4,6]           // Diagonalen
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
   ];
 
   for (let combo of winCombos) {
     const [a, b, c] = combo;
     if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      // Markiere Gewinnerzellen
+      combo.forEach(i => cells[i].classList.add("win"));
       return board[a]; // X oder O
     }
   }
@@ -48,6 +50,7 @@ function checkWinner() {
 
   return null;
 }
+
 
 // --------- EVENTS ---------
 
